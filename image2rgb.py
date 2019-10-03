75bc61114f0248458dc6860a547fc3843becc8fe
Tobias Borgert <tobias.borgert@gmail.com>
2019-10-03 19:42:11 +0200

    Initial commit of main Python file. Currently delivering wrong results.

---
+++ image2rgb.py
@@ -0,0 +1,68 @@
+import sys
+from PyQt5.QtGui import QImage, qRed, qGreen, qBlue
+
+
+def get_16bit_color_from_rgb(red, green, blue):
+    """
+    Get a 16bit color string (0x...) from RGB values.
+    :param red: 0..255
+    :param green: 0..255
+    :param blue: 0..255
+    :return:  A string with the 16bit value in hex format.
+    """
+    red_16bit = (red//31 * 255) << 11
+    green_16bit = (green//63 * 255) << 5
+    blue_16bit = (blue//31 * 255)
+    rgb_16bit = red_16bit | green_16bit | blue_16bit
+    return '0x{0:04x}'.format(rgb_16bit)
+
+def image_to_rgb_array(input_path, output_path, variable_name='rgb_array'):
+    """
+    Converts an image to an unsigned char array containing RGB values.
+    :param input_path: Path to the image.
+    :param output_path: Output path.
+    :param variable_name: Name for the array variable.
+    """
+    image = QImage(input_path)
+    with open(output_path, 'w') as file_:
+        # Write the first line of the file, declaring an array with the size of the image's pixel count and the given
+        # name.
+        file_.write('const uint16_t {}[{}] = {}\n\t'.format(variable_name, image.width() * image.height(), '{'))
+        for y in range(0, image.width()):
+            for x in range(0, image.height()):
+                pixel = image.pixel(x, y)
+                if x == image.width() - 1 and y == image.height() - 1:
+                    # Last pixel in image: Close the array with '}' and add a semicolon.
+                    if x == 0:
+                        # Catches the edge case of an image with width 1 and ensures correct file formatting in that
+                        # case.
+                        file_.write('{}{};'.format(get_16bit_color_from_rgb(qRed(pixel),
+                                                                            qGreen(pixel),
+                                                                            qBlue(pixel)), '}'))
+                    else:
+                        # Last pixel of an image with a width larger than 0.
+                        file_.write(' {}{};'.format(get_16bit_color_from_rgb(qRed(pixel),
+                                                                             qGreen(pixel),
+                                                                             qBlue(pixel)), '}'))
+                elif x == 0:
+                    # First pixel in a line.
+                    file_.write('{},'.format(get_16bit_color_from_rgb(qRed(pixel),
+                                                                      qGreen(pixel),
+                                                                      qBlue(pixel))))
+                else:
+                    # All other pixels.
+                    file_.write(' {},'.format(get_16bit_color_from_rgb(qRed(pixel),
+                                                                       qGreen(pixel),
+                                                                       qBlue(pixel))))
+            if y < image.height() - 1:
+                # If more lines follow, add a line break and an initial tab stop.
+                file_.write('\n\t')
+        # Add an empty line to the end of the file.
+        file_.write('\n')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 3:
+        image_to_rgb_array(sys.argv[1], sys.argv[2], sys.argv[3])
+    else:
+        image_to_rgb_array(sys.argv[1], sys.argv[2])
